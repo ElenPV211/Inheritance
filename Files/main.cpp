55fd5fed2d6c7e3e95c4bb302d7a424e803697de
user <user@DESKTOP-LI69LEU>
2023-03-10 18:41:33 +0300

    sohranenie

--- Files/main.cpp
+++ Files/main.cpp
@@ -15,7 +15,20 @@ void main()
 	cout << endl;
 	//https://legacy.cplusplus.com/doc/tutorial/files/
 #ifdef WRITE_TO_FILE
-    ofstream fout; //1) Создаём поток
+
+	name_file[256];
+	cout << "Введите имя файла: \n";
+	cin >> name_file;
+
+	if ()//во введённом имени нет .txt делаем name_file = 'name_file'+'.txt'
+
+	ofstream fout;
+	fout.open(name_file, std::ios_base::app); 
+	fout << "Всем привет" << endl;
+	fout << "Hello Files" << endl;//3) Пишем в поток
+	fout.close(); // 4) После того как запись в поток*/
+
+    //1) Создаём поток
 	fout.open("File.txt", std::ios_base::app); //2) Открываем поток
 	fout << "Всем привет" << endl;
 	fout << "Hello Files" << endl;//3) Пишем в поток
@@ -24,6 +37,8 @@ void main()
 	system("notepad File.txt");
 #endif // WRITE_TO_FILE
 
+
+#ifdef READ_FROM_FILE
 	std::ifstream fin("File.txt");
 	cout << "Начальная позиция курсора: " << fin.tellg() << endl;
 	fin.seekg(0, ios::end);
@@ -55,4 +70,5 @@ void main()
 	{
 		std::cerr << "Error: file not found" << endl;
 	}
+#endif  // READ_FROM_FILE
 }
